8bde09b278a12ac52c835f5c785fdad08b9ab35e
robegonza13-pixel <robegonza13@gmail.com>
2026-08-14 16:27:45 -0600

    Se aplican cambios a GestorDatos.py

--- src/datos/GestorDatos.py
+++ src/datos/GestorDatos.py
@@ -26,18 +26,103 @@ class GestorDatos:
             index=False,
             encoding="utf-8"
         )
+    @staticmethod
+    def limpiar_clima(df, ubicacion):
+        df = df.copy()
 
-    def limpiar_clima(self, df):
-        # limpieza específica del clima
-        return df
+        df.columns = ["fecha", "temperatura", "humedad", "velocidad_viento", "latitud", "longitud"]
 
-    def limpiar_calidad_aire(self, df):
-        # limpieza específica del aire
-        return df
+        df["fecha"] = pd.to_datetime(df["fecha"])
 
-    def limpiar_congestion(self, df):
-        # limpieza específica del tráfico
-        return df
+        df["hora"] = df["fecha"].dt.hour
+        df["dia_semana"] = df["fecha"].dt.dayofweek
 
-    def unir_datos(df_clima, df_calidad_aire, df_congestion)
-        return df
+        df["ubicacion"] = ubicacion
+
+        columnas = [
+            "fecha",
+            "hora",
+            "dia_semana",
+            "ubicacion",
+            "temperatura",
+            "humedad",
+            "velocidad_viento",
+            "latitud",
+            "longitud"
+        ]
+
+        return df[columnas]
+
+    @staticmethod
+    def limpiar_calidad_aire(df, ubicacion):
+        df = df.copy()
+
+        df.columns = ["fecha", "pm_10", "pm_2_5", "no2", "o3", "latitud", "longitud"]
+
+        df["fecha"] = pd.to_datetime(df["fecha"])
+
+        df["hora"] = df["fecha"].dt.hour
+        df["dia_semana"] = df["fecha"].dt.dayofweek
+
+        df["ubicacion"] = ubicacion
+
+        columnas = [
+            "fecha",
+            "hora",
+            "dia_semana",
+            "ubicacion"
+            "pm_10",
+            "pm_2_5",
+            "no2",
+            "o3",
+            "latitud",
+            "longitud"
+        ]
+
+        return df[columnas]
+
+    @staticmethod
+    def limpiar_congestion(df, ubicacion):
+        df = df.copy()
+
+        df.columns = ["origen_lat", "origen_lon", "destino_lat", "destino_lon", "departure_time", "duracion_normal_seg",
+            "duracion_trafico_seg", "congestion_ratio"]
+
+        df["fecha"] = pd.to_datetime(df["departure_time"], utc=True)
+
+        df["hora"] = df["fecha"].dt.hour
+        df["dia_semana"] = df["fecha"].dt.dayofweek
+
+        df["ubicacion"] = ubicacion
+
+        df["congestion_ajustada"] = df["congestion_ratio"].clip(lower=1)
+
+        df["nivel_congestion"] = pd.cut(
+            df["congestion_ajustada"],
+            bins=[0, 1, 1.3, 1.7, float("inf")],
+            labels=["Fluido", "Leve", "Moderado", "Pesado"],
+            include_lowest=True
+        )
+
+        columnas = [
+            "hora",
+            "dia_semana",
+            "ubicacion",
+            "origen_lat",
+            "origen_lon",
+            "destino_lat",
+            "destino_lon",
+            "duracion_normal_seg",
+            "duracion_trafico_seg",
+            "congestion_ratio",
+            "congestion_ajustada",
+            "nivel_congestion"
+        ]
+
+        return df[columnas]
+
+    @staticmethod
+    def unir_datos(df_clima, df_calidad_aire, df_congestion):
+        df = pd.merge(df_clima, df_calidad_aire, on=["fecha", "hora", "dia_semana", "latitud", "longitud"], how="left")
+        df_final = pd.merge(df, df_congestion, on=["hora", "dia_semana"], how="left")
+        return df_final
